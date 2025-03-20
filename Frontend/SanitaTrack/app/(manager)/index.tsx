ae02af3e13c3d0200bc94122fb49e5757283da09
bengisukayan <bengisukayan@gmail.com>
2025-03-20 23:30:19 +0300

    deneme

--- Frontend/SanitaTrack/app/(manager)/index.tsx
+++ Frontend/SanitaTrack/app/(manager)/index.tsx
@@ -17,7 +17,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title" >Melike!!</ThemedText>
+        <ThemedText type="title" >Melikooo!!</ThemedText>
         <HelloWave />
       </ThemedView>
       <Link href="/taskpage" >
