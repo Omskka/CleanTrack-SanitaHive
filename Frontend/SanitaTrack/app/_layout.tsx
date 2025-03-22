b6009d773abd40b0c3e8012eb79bcc9a334f1878
bengisukayan <bengisukayan@gmail.com>
2025-03-23 00:16:30 +0300

    Removed light/dark theme configiration at _layout

--- Frontend/SanitaTrack/app/_layout.tsx
+++ Frontend/SanitaTrack/app/_layout.tsx
@@ -1,4 +1,3 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
@@ -8,13 +7,10 @@ import 'react-native-reanimated';
 import { GluestackUIProvider } from "@gluestack-ui/themed"
 import { config } from "@gluestack-ui/config"
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
 // the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
@@ -40,7 +36,7 @@ export default function RootLayout() {
         <Stack.Screen name="feedback" />
         <Stack.Screen name="+not-found" />
       </Stack>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
     </GluestackUIProvider>
   );
 }
